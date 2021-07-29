38f8bc44c0298be5a70afce2fb93ddde3919eb1f
Abed Rayyan <abedrayyan18@gmail.com>
2021-07-29 20:27:10 +0100

    first commit

---
+++ cmake/Conan.cmake
@@ -0,0 +1,30 @@
+macro(run_conan)
+  # Download automatically, you can also just copy the conan.cmake file
+  if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
+    message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
+    file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/v0.15/conan.cmake" "${CMAKE_BINARY_DIR}/conan.cmake")
+  endif()
+
+  include(${CMAKE_BINARY_DIR}/conan.cmake)
+
+
+  conan_add_remote(
+    NAME
+    bincrafters
+    URL
+    https://api.bintray.com/conan/bincrafters/public-conan)
+
+  conan_cmake_run(
+    REQUIRES
+    ${CONAN_EXTRA_REQUIRES}
+    spdlog/1.8.0
+    nlohmann_json/3.9.1
+    opencv/4.3.0@conan/stable
+    OPTIONS
+    ${CONAN_EXTRA_OPTIONS}
+    BASIC_SETUP
+    KEEP_RPATHS
+    CMAKE_TARGETS
+    BUILD
+    missing)
+endmacro()
